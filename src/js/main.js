a29683f591417ff41d48966b55564eef08fe876c
Marta Torres Burguillo <99444191+burguimarta@users.noreply.github.com>
2022-04-08 12:54:10 +0200

    Finish step 2: add image placeholder with if

--- src/js/main.js
+++ src/js/main.js
@@ -6,23 +6,25 @@ const list = document.querySelector('.js_list');
 document.querySelector('.js-searchbtn').addEventListener('click', function(event) {
   event.preventDefault();
   //Vaciar lista del HTML
-  list.innerHTML = "";
+  list.innerHTML = '';
+  // Interpolada variable con el value del Input
 
   fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${coctelInput.value}`)
     .then((response) => response.json())
     .then((data) => {
-      
-      for (var drink of data.drinks)
-      {
-        console.log(drink['strDrink']);
+      for (let drink of data.drinks) {
+        const drinkName = drink['strDrink'];
+        let image = drink['strDrinkThumb'];
 
+        if (image === '') {
+          image = 'https://via.placeholder.com/150';
+        }
         //DOM basico
-        list.innerHTML += `<div class="card">
-        <h3>${drink['strDrink']}</h3>
-        <div class="container__image">
-          <img src="${}">
-        </div>
-      </div>`;
+        list.innerHTML += `<div class="cocktail__card">
+          <h3 class="cocktail__name">${drinkName}</h3>
+          <div>
+          <img class="cocktail__image" src="${image}">
+          </div>`;
 
         // //DOM avanzado
         // const drinkLi = document.createElement('li');
